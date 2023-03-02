47d6aec7e85a274f8ce353f4bb15c61d7bae472f
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-03-02 12:10:16 +0100

    Made a simple 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,11 @@
-<h1>404</h1>
+<script>
+    import LinkButton from "$lib/components/linkButton.svelte";
+</script>
+
+<div class="flex justify-center items-center flex-col h-screen">
+    <h1 class="font-medium text-8xl text-blue-500">404</h1>
+    <p class="text-lg text-gray-600 text-center px-4 pb-4">
+        Úgy néz ki ez az oldal nem található. Lehetséges, hogy elírta az oldal címét!
+    </p>
+    <LinkButton href="/" text="Ugrás a kezdőlapra" />
+</div>
