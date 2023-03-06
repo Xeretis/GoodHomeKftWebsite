a2dcd47b447b0e2ad246f415652da9bc7edbdda8
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-03-06 10:13:52 +0100

    Added some decoration to the error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,4 +8,13 @@
         Úgy néz ki ez az oldal nem található. Lehetséges, hogy elírta az oldal címét!
     </p>
     <LinkButton href="/" text="Ugrás a kezdőlapra" />
+    <div class="dottedBg absolute bottom-4 right-4 w-[448px] h-72 z-10 max-lg:hidden" />
+    <div class="dottedBg absolute top-8 left-8 w-32 h-32 z-10 max-sm:hidden" />
 </div>
+
+<style>
+    .dottedBg {
+        background-image: radial-gradient(#93c5fd 3px, transparent 0);
+        background-size: 40px 40px;
+    }
+</style>
